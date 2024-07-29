c8429f57009003a5a2a9234e9b6bf52ed2564ec5
Jack Wilburn <jackwilburn@tutanota.com>
2024-07-29 14:11:00 -0600

    Add suggested fix from readthedocs email

--- conf.py
+++ conf.py
@@ -6,6 +6,17 @@ list see the documentation:
 http://www.sphinx-doc.org/en/master/config
 """
 
+import os
+
+# Set canonical URL from the Read the Docs Domain
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
+
+html_context = {} if html_context is None else html_context
+
+# Tell Jinja2 templates the build is running on Read the Docs
+if os.environ.get("READTHEDOCS", "") == "True":
+    html_context["READTHEDOCS"] = True
+
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
